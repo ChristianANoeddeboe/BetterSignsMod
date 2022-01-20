6322be58e56262d8d2537ca56eb59732eb127ae0
can <can@vaerks.com>
2022-01-20 14:08:51 +0100

    Updated to 1.18.1

--- src/main/java/angercraft/bettersigns/config/Keybinds.java
+++ src/main/java/angercraft/bettersigns/config/Keybinds.java
@@ -1,12 +1,12 @@
 package angercraft.bettersigns.config;
 
 import angercraft.bettersigns.BetterSigns;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.client.util.InputMappings;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
@@ -14,7 +14,7 @@ import org.lwjgl.glfw.GLFW;
 @Mod.EventBusSubscriber(modid = BetterSigns.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Keybinds {
 
-    public static KeyBinding noSignGui = new KeyBinding("key.bettersigns.nosigntext", KeyConflictContext.UNIVERSAL, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, "key.categories.bettersigns");
+    public static KeyMapping noSignGui = new KeyMapping("key.bettersigns.nosigntext", KeyConflictContext.UNIVERSAL, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, "key.categories.bettersigns");
 
     @SubscribeEvent
     public static void registerKeyBindings(final FMLClientSetupEvent event) {
